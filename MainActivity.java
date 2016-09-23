fc4bb0d820d98e461c5b7c090305ee4fe359901a
Jonathan SCHOTS ALSUGUREN <jonathan.schotsalsuguren@greta-sud-aquitaine.fr>
2016-09-23 09:35:04 +0200

    1er commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.stagiaires.speedymarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
