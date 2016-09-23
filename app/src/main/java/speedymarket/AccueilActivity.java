d39ca057f903e0918cea22ccd49185c4401fb8c7
Jonathan SCHOTS ALSUGUREN <jonathan.schotsalsuguren@greta-sud-aquitaine.fr>
2016-09-23 16:55:04 +0200

    Fix arborescence

--- app/src/main/java/speedymarket/AccueilActivity.java
+++
@@ -1,8 +0,0 @@
-package speedymarket;
-
-/**
- * Created by Karim on 23/09/2016.
- */
-
-public class AccueilActivity {
-}
--- app/src/main/java/speedymarket/ArticleActivity.java
+++
@@ -1,9 +0,0 @@
-package speedymarket;
-
-/**
- * Created by Karim on 23/09/2016.
- */
-
-public class ArticleActivity {
-
-}
